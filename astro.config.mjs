fb6c3e707f2a53636e6b4e8431af6b96cfc3871d
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-08-11 16:48:03 +0700

    chore: astro domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://promatcon.lun.web.id',
   vite: {
     plugins: [tailwindcss()],
   },
