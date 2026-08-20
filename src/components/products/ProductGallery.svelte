3cf2c2c60dfea76ee3e17c3e39e3c4e1667fbdba
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-08-20 18:46:14 +0700

    fix: icons and language toggle button

--- src/components/products/ProductGallery.svelte
+++ src/components/products/ProductGallery.svelte
@@ -14,7 +14,8 @@
     faWeightHanging,
     faCogs,
     faCut,
-    faBolt
+    faBolt,
+    faMagnifyingGlassPlus
   } from "@fortawesome/free-solid-svg-icons";
 
   const gasCompressorImages = [
@@ -142,9 +143,7 @@
                       {product.title}
                   </h3>
                   <div class="mt-2 w-8 h-8 rounded-full bg-primary flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity delay-100">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4 text-primary-content">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                      </svg>
+                      <Fa icon={faMagnifyingGlassPlus} class="w-4 h-4 text-primary-content" />
                   </div>
               </div>
           </button>
